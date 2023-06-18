e1f03989b10ad1dd4d2eae5faceafe4996eff738
binger117 <mrbinger117@gmail.com>
2023-06-18 08:35:42 +0800

    commit：添加商品路由文件，以及商品图片

--- web02/web02/urls.py
+++ web02/web02/urls.py
@@ -1,7 +1,8 @@
-"""web02_shop URL Configuration
+"""
+URL configuration for web02 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,20 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-# 获取文件的视图
 from users.views import FileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 获取图片资源的接口
     re_path(r'file/image/(.+?)/', FileView.as_view()),
-    # 用户模块接口
     path('api/users/', include('users.urls')),
-    # 商品模块接口
-    path('api/goods/', include('goods.urls')),
-    # 购物车模块接口
-    #path('api/cart/', include('cart.urls')),
-    # 订单模块
-    #path('api/order/', include('order.urls')),
-
 ]
